2388aa339791e083a54ae4c3562733132736e13a
whimzyLive <rp970.patel@gmail.com>
2020-11-25 18:22:35 +1030

    chore: apply strict scoped commit naming rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,17 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+const {readdirSync} = require('fs');
+
+const packages = () =>
+  readdirSync('./packages', {withFileTypes: true})
+    .filter(dir => dir.isDirectory())
+    .map(dir => dir.name);
+
+const validScopes = ['examples', ...packages()];
+console.log('Valid Package scopes:', validScopes);
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', validScopes],
+    'scope-case': [2, 'always', 'kebab-case'],
+  },
+};
