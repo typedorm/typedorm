11cf00365883e16bdc0676ccee3281d99411795c
whimzyLive <rp970.patel@gmail.com>
2020-11-27 19:23:12 +1030

    chore: collect code coverage after tests run

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
