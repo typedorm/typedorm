eceb3febc7911fb487bac2436350a254094ecb34
whimzyLive <rp970.patel@gmail.com>
2020-12-03 17:41:48 +1030

    test(core): add more test coverage for inherited attributes

--- packages/core/classes/metadata/__test__/attribute-metadata.spec.ts
+++ packages/core/classes/metadata/__test__/attribute-metadata.spec.ts
@@ -1,21 +1,7 @@
-import {Connection} from '../../connection/connection';
-import {createTestConnection, resetTestConnection} from '@typedorm/testing';
 import {AttributeMetadata} from '../attribute-metadata';
-
-let connection: Connection;
-beforeEach(() => {
-  connection = createTestConnection({
-    entities: [],
-  });
-});
-
-afterEach(() => {
-  resetTestConnection();
-});
 
 test('creates attribute metadata', () => {
   const attMetadata = new AttributeMetadata({
-    connection,
     name: 'id',
     type: 'String',
   });
