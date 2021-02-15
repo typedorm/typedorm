dd07d64b85ac2c6c2d9ba41d4dea3defa0389943
whimzyLive <rp970.patel@gmail.com>
2021-02-15 17:56:17 +1030

    refactor(core): better organize transforming for batches

--- packages/core/src/classes/batch/batch.ts
+++ packages/core/src/classes/batch/batch.ts
@@ -1,14 +1,10 @@
-import {DynamoDB} from 'aws-sdk';
 import {Connection} from '../connection/connection';
 import {DocumentClientRequestTransformer} from '../transformer/document-client-request-transformer';
 
 export abstract class Batch {
-  protected _items: DynamoDB.DocumentClient.BatchWriteItemInput[];
-
   protected _dcRequestTransformer: DocumentClientRequestTransformer;
 
   constructor(connection: Connection) {
-    this._items = [];
     this._dcRequestTransformer = new DocumentClientRequestTransformer(
       connection
     );
