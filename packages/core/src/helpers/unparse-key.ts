38a6cf4f0c49bf0c5a259e3ebdc0d4914aeae704
whimzyLive <rp970.patel@gmail.com>
2021-02-22 12:57:51 +1030

    feat(core): add helpers to reverse parse key to key attributes

--- packages/core/src/helpers/unparse-key.ts
+++ packages/core/src/helpers/unparse-key.ts
@@ -1,21 +1,14 @@
+import {buildRegexForKeyMatch} from './build-regex-for-key-match';
+
 export function unParseKey(
   keySchema: string,
   keyToUnParse: string,
   interpolations: string[]
 ) {
-  const varsPattern = interpolations.join('|');
-  const keysMatcher = new RegExp(`\\{{(${varsPattern})\\}}`, 'gm');
-
-  // replace keys {{id}} with (.*) as matchers
-  const keys = [] as string[];
-  keySchema = keySchema.replace(keysMatcher, substr => {
-    const matchKey = (substr.match(/\{{(.+?)\}}/) ?? [])[1];
-    keys.push(matchKey);
-    return '(.*)';
-  });
-
-  // build regex from new key schema
-  const keySchemaExp = new RegExp(`^${keySchema}`, 'gm');
+  const {exp: keySchemaExp, keys} = buildRegexForKeyMatch(
+    keySchema,
+    interpolations
+  );
 
   // first match is the full string, rest will be the values for variables
   const values = keySchemaExp.exec(keyToUnParse)?.splice(1) ?? ([] as any[]);
