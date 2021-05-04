19be49b179bb6d58425ad661b0dcb25220acb9c6
whimzyLive <rp970.patel@gmail.com>
2021-05-04 19:28:47 +0930

    feat(core): wip: initial attribute aliasing support

--- packages/common/private-api.ts
+++ packages/common/private-api.ts
@@ -2,3 +2,4 @@ export * from './src/metadata/metadata-manager';
 export * from './src/metadata/metadata-storage';
 export * from './src/logger/debug-logger';
 export * from './src/error';
+export * from './src/index-options';
