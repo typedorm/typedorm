c6e0f93089d4cfefb49eb2d3fac3cf34126391bd
Andrei <andrei@palchys.com>
2021-09-08 10:54:06 +0300

    fix(core): correctly detect scalar types

--- packages/core/src/helpers/is-scalar-type.ts
+++ packages/core/src/helpers/is-scalar-type.ts
@@ -1,11 +1,10 @@
 import {ScalarType} from '@typedorm/common';
 
 export const isScalarType = (item: any): item is ScalarType =>
-  item &&
-  (typeof item === 'string' ||
-    typeof item === 'number' ||
-    typeof item === 'boolean' ||
-    Buffer.isBuffer(item));
+  typeof item === 'string' ||
+  (typeof item === 'number' && !isNaN(item)) ||
+  typeof item === 'boolean' ||
+  Buffer.isBuffer(item);
 
 export const isScalarTypeProvider = (item: any): item is () => ScalarType =>
   item && typeof item === 'function';
