03f4343cf53abdee2d32b60563d13411f2b296ea
Rushi Patel <rp970.patel@gmail.com>
2023-03-26 01:34:29 +1030

    feat(document-client): expose translateConfig options to DocumentClientV3

---
+++ packages/document-client/src/constants/translate-config.ts
@@ -0,0 +1,14 @@
+export const DEFAULT_TRANSLATE_CONFIG_V3 = {
+  marshallOptions: {
+    // Whether to automatically convert empty strings, blobs, and sets to `null`.
+    convertEmptyValues: false, // false, by default.
+    // Whether to remove undefined values while marshalling.
+    removeUndefinedValues: false, // false, by default.
+    // Whether to convert typeof object to map attribute.
+    convertClassInstanceToMap: false, // false, by default.
+  },
+  unmarshallOptions: {
+    // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+    wrapNumbers: false, // false, by default.
+  },
+};
