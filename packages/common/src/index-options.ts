9f837da399306cb319078cb3b18617321d0c5fd5
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-09-17 20:37:33 +0200

    Rollback whitespace changes in index-options

--- packages/common/src/index-options.ts
+++ packages/common/src/index-options.ts
@@ -1,5 +1,5 @@
-import { INDEX_TYPE } from './enums';
-import { Replace } from './helpers/replace-type';
+import {INDEX_TYPE} from './enums';
+import {Replace} from './helpers/replace-type';
 
 /** 
   * When a Global Secondary Index(GSI) is created with both a partition key and a sort key, any items added * to the table that lack a partition key or sort key value will be excluded from the index.
@@ -28,17 +28,17 @@ export type EntityAliasOrString<Entity> = string | KeyAliasSchema<Entity>;
 
 export type IndexOptionsWithAlias<Entity> =
   | Replace<
-    GSIIndexOptions,
-    'partitionKey' | 'sortKey',
-    {
-      partitionKey: EntityAliasOrString<Entity>;
-      sortKey: EntityAliasOrString<Entity>;
-    }
-  >
+      GSIIndexOptions,
+      'partitionKey' | 'sortKey',
+      {
+        partitionKey: EntityAliasOrString<Entity>;
+        sortKey: EntityAliasOrString<Entity>;
+      }
+    >
   | Replace<
-    LSIIndexOptions,
-    'sortKey',
-    {
-      sortKey: EntityAliasOrString<Entity>;
-    }
-  >;
+      LSIIndexOptions,
+      'sortKey',
+      {
+        sortKey: EntityAliasOrString<Entity>;
+      }
+    >;
