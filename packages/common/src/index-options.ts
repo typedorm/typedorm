ca7ed663715543ce8bad24c0c84591c5547c99d7
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-09-17 20:17:39 +0200

    Add comment

--- packages/common/src/index-options.ts
+++ packages/common/src/index-options.ts
@@ -1,6 +1,12 @@
-import {INDEX_TYPE} from './enums';
-import {Replace} from './helpers/replace-type';
+import { INDEX_TYPE } from './enums';
+import { Replace } from './helpers/replace-type';
 
+/** 
+  * When a Global Secondary Index(GSI) is created with both a partition key and a sort key, any items added * to the table that lack a partition key or sort key value will be excluded from the index.
+  * For GSIs that only have a partition key, the same behaviour applies, but only to the partition key.
+  * 
+  * In these scenarios `isSparse` should be set to True.
+  */
 export interface GSIIndexOptions {
   type: INDEX_TYPE.GSI;
   partitionKey: string;
@@ -22,17 +28,17 @@ export type EntityAliasOrString<Entity> = string | KeyAliasSchema<Entity>;
 
 export type IndexOptionsWithAlias<Entity> =
   | Replace<
-      GSIIndexOptions,
-      'partitionKey' | 'sortKey',
-      {
-        partitionKey: EntityAliasOrString<Entity>;
-        sortKey: EntityAliasOrString<Entity>;
-      }
-    >
+    GSIIndexOptions,
+    'partitionKey' | 'sortKey',
+    {
+      partitionKey: EntityAliasOrString<Entity>;
+      sortKey: EntityAliasOrString<Entity>;
+    }
+  >
   | Replace<
-      LSIIndexOptions,
-      'sortKey',
-      {
-        sortKey: EntityAliasOrString<Entity>;
-      }
-    >;
+    LSIIndexOptions,
+    'sortKey',
+    {
+      sortKey: EntityAliasOrString<Entity>;
+    }
+  >;
